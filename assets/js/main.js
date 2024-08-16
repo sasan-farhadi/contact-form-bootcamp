cebdb46feb9769a4b1227dd4f79d40ee55d81d2b
sasanfarhadi <sasanfarhadi@gmail.com>
2024-08-16 16:17:40 +0330

    validate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,105 @@
 const mode = document.getElementById('mode')
 const star = document.querySelector('body')
 const contact = document.getElementById('contact')
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const general = document.getElementById('color-1')
+const support = document.getElementById('color-2')
+const message = document.getElementById('message')
+const check = document.getElementById('check')
+const sectionQuery = document.querySelector('.query')
+
+
 
 form.addEventListener('submit', (e) => {
+    console.log(check.checked, support.checked, general.checked);
     if (!validateInputs()) {
         e.preventDefault();
     }
 })
+
+
+const validateInputs = () => {
+    const fnameVal = fname.value.trim()
+    const lnameVal = lname.value.trim()
+    const emailVal = email.value.trim()
+    const messageVal = message.value.trim()
+    const checkVal = check.checked
+    const generalVal = general.checked
+    const supportVal = support.checked
+    let success = true
+
+    if (fnameVal === '') {
+        success = false
+        setError(fname, "This field is required")
+    } else {
+        setSuccess(fname)
+    }
+
+    if (lnameVal === '') {
+        success = false
+        setError(lname, "This field is required")
+    } else {
+        setSuccess(lname)
+    }
+
+    if (emailVal === '') {
+        success = false;
+        setError(email, 'Email is required')
+    }
+    else if (!validateEmail(emailVal)) {
+        success = false;
+        setError(email, 'Please enter a valid email')
+    }
+    else {
+        setSuccess(email)
+    }
+
+    if (messageVal === '') {
+        success = false
+        setError(message, "This field is required")
+    } else {
+        setSuccess(message)
+    }
+
+
+    if (checkVal == false) {
+        success = false
+        setError(check, "This field is required")
+    } else {
+        setSuccess(check)
+    }
+
+    if (generalVal == false && supportVal == false) {
+        success = false
+        setError(sectionQuery, "This field is required")
+    } else {
+        setSuccess(sectionQuery)
+    }
+}
+
+
+
+
+const setError = (element, message) => {
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = message
+    inputGroup.classList.add('error')
+    inputGroup.classList.remove('success')
+}
+
+
+function setSuccess(element) {
+    const inputGroup = element.parentElement;
+    const errorElement = inputGroup.querySelector('.error')
+
+    errorElement.innerText = '';
+    inputGroup.classList.add('success')
+    inputGroup.classList.remove('error')
+}
 
 
 const modeHandler = () => {
@@ -22,6 +115,15 @@ const modeHandler = () => {
         contact.style.boxShadow = "-2px 7px 16px 5px black"
     }
 }
+
+
+const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+};
 
 mode.addEventListener("click", modeHandler)
 
